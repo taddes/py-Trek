f74208db29afd79c6e50a86bc00966a3772eb2b3
Taddes Korris <taddes.korris@gmail.com>
2019-03-17 22:06:08 -0400

    basic contest logic

--- game.py
+++ game.py
@@ -6,5 +6,30 @@ player = {'name': 'Captain Kirk', 'attack': 10, 'heal': 16, 'health': 100}
 
 enemy = {'name': 'Captain Kor', 'attack': 12, 'health': 100}
 
+game_running = True
 
-print(player)
+while game_running == True:
+    print('Select an action')
+    print('1) Attack')
+    print('2) Heal')
+    player_choice = input()
+
+    if player_choice == '1':
+        enemy['health'] = enemy['health'] - player['attack']
+        if enemy['health'] <= 0:
+            player_won = True
+        else:
+            player['health'] = player['health'] - enemy['attack']
+            if player['health'] <= 0:
+                enemy_won = True
+
+        print(enemy['health'])
+        print(player['health'])
+
+    elif player_choice == '2':
+        print('Heal')
+    else:
+        print('Invalid input')
+
+    if player_won == True or enemy_won == True:
+        game_running = False
