a09f08e86b1403775537f9b6e59c18aa8055f9cf
Taddes Korris <taddes.korris@gmail.com>
2019-03-17 21:39:03 -0400

    basic setup

---
+++ game.py
@@ -0,0 +1,10 @@
+"""
+    A Star Trek inspired game to pit your Starfleet officers against the enemies of the Federation!
+"""
+
+player = {'name': 'Captain Kirk', 'attack': 10, 'heal': 16, 'health': 100}
+
+enemy = {'name': 'Captain Kor', 'attack': 12, 'health': 100}
+
+
+print(player)
